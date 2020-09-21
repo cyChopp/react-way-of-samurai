c8ccc01180e1154cc4cf2e248ae1fbc1b4e56baa
cyChopp <45396059+cyChopp@users.noreply.github.com>
2020-09-21 20:39:11 +0300

    37. Binding object methods

--- src/index.js
+++ src/index.js
@@ -3,23 +3,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import state, { addDialogMessage, addMessage, subscribe, updateNewDialogMessage, updateNewPostChange } from "./redux/state";
+import store from "./redux/state";
 import { BrowserRouter } from "react-router-dom";
 
 
- let rerenderEntireTree = () => {
+ let rerenderEntireTree = (state) => {
     ReactDOM.render(
         <BrowserRouter>
             <App     state={state} 
-                     addMessage={addMessage}
-                     updateNewPostChange={updateNewPostChange}
-                     updateNewDialogMessage={updateNewDialogMessage}
-                     addDialogMessage={addDialogMessage}
+                     addMessage={store.addMessage.bind(store)}
+                     updateNewPostChange={store.updateNewPostChange.bind(store)}
+                     updateNewDialogMessage={store.updateNewDialogMessage.bind(store)}
+                     addDialogMessage={store.addDialogMessage.bind(store)}
             />
         </BrowserRouter>, document.getElementById('root'));
 }
-rerenderEntireTree();
-subscribe(rerenderEntireTree);
+rerenderEntireTree(store.getState());
+store.subscribe(rerenderEntireTree);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,82 +1,88 @@
- let rerenderEntireTree = ()=>{
-     console.log('changed');
-}
 
-let state = {
-    profilePage: {
-        posts: [
-            { id: 1, message: 'Hi, how are you?', likesCount: 12 },
-            { id: 2, message: 'It\'s my first post', likesCount: 11 },
-            { id: 3, message: 'Blabla', likesCount: 11 },
-            { id: 4, message: 'Dada', likesCount: 11 }
-        ],
-        newPostText:''
+
+let store = {
+
+    _state: {
+        profilePage: {
+            posts: [
+                { id: 1, message: 'Hi, how are you?', likesCount: 12 },
+                { id: 2, message: 'It\'s my first post', likesCount: 11 },
+                { id: 3, message: 'Blabla', likesCount: 11 },
+                { id: 4, message: 'Dada', likesCount: 11 }
+            ],
+            newPostText: ''
+
+        },
+
+        dialogsPage: {
+            messages: [
+                { id: 1, message: 'Hi' },
+                { id: 2, message: 'How is your it-kamasutra?' },
+                { id: 3, message: 'Yo' },
+                { id: 4, message: 'Yo Yo' },
+                { id: 5, message: 'Yo Yo Yo' },
+                { id: 6, message: 'Yo Yo Yo Yo' }
+            ],
+            newDialogMessage: '',
+
+            dialogs: [
+                { id: 1, name: 'Dimych' },
+                { id: 2, name: 'Andrew' },
+                { id: 3, name: 'Sveta' },
+                { id: 4, name: 'Sasha' },
+                { id: 5, name: 'Viktor' },
+                { id: 6, name: 'Valera' }
+            ],
+            newDialog: '',
+        },
 
     },
 
-    dialogsPage: {
-        messages: [
-            { id: 1, message: 'Hi' },
-            { id: 2, message: 'How is your it-kamasutra?' },
-            { id: 3, message: 'Yo' },
-            { id: 4, message: 'Yo Yo' },
-            { id: 5, message: 'Yo Yo Yo' },
-            { id: 6, message: 'Yo Yo Yo Yo' }
-        ],
-        newDialogMessage:'',
-
-        dialogs: [
-            { id: 1, name: 'Dimych' },
-            { id: 2, name: 'Andrew' },
-            { id: 3, name: 'Sveta' },
-            { id: 4, name: 'Sasha' },
-            { id: 5, name: 'Viktor' },
-            { id: 6, name: 'Valera' }
-        ],
-        newDialog:'',
+    _callSubscriber(){
+        console.log('changed');
+    },
+    getState() {
+        return this._state;
+    },
+    /*  --START-- Main logic of Adding messages and dialogs */
+    addDialogMessage() {
+        let newMessage = {
+            id: 7,
+            message: this._state.dialogsPage.newDialogMessage
+        };
+        this._state.dialogsPage.messages.push(newMessage);
+        this._state.dialogsPage.newDialogMessage = '';
+        this._callSubscriber(this._state);
     },
 
-};
+    updateNewDialogMessage(dialogText) {
+        this._state.dialogsPage.newDialogMessage = dialogText;
+        this._callSubscriber(this._state);
+    },
 
-/*  --START-- Main logic of Adding messages and dialogs */
-export let addDialogMessage = ()=>{
-    let newMessage = {
-        id:7,
-        message:state.dialogsPage.newDialogMessage
-    };
-    state.dialogsPage.messages.push(newMessage);
-    state.dialogsPage.newDialogMessage='';
-    rerenderEntireTree(state);
-};
+    addMessage() {
+        let newPost = {
+            id: 5,
+            message: this._state.profilePage.newPostText,
+            likesCount: 0
+        };
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.newPostText = "";
+        this._callSubscriber(this._state);
+    },
 
-export let updateNewDialogMessage = (dialogText)=>{
-    state.dialogsPage.newDialogMessage= dialogText;
-    rerenderEntireTree(state);
+    updateNewPostChange(newText) {
 
+        this._state.profilePage.newPostText = newText;
+       this._callSubscriber(this._state);
+    },
 
-}
-
-export let addMessage =() =>{
-    let newPost={
-        id:5,
-        message:state.profilePage.newPostText,
-        likesCount:0
+    subscribe(observer) {
+        this._callSubscriber = observer;
     }
-    state.profilePage.posts.push(newPost);
-    state.profilePage.newPostText="";
-    rerenderEntireTree(state);
-};
+    /*  --END-- Main logic of Adding messages and dialogs */
 
-export let updateNewPostChange =(newText) =>{
-    
-    state.profilePage.newPostText=newText;
-    rerenderEntireTree(state);
-};
-/*  --END-- Main logic of Adding messages and dialogs */
+};       
 
-
-export const subscribe = (observer)=>{
-    rerenderEntireTree=observer;
-}
-
-export default state;
+export default store;
+window.store = store;
